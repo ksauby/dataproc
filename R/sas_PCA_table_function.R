fce0646ca9c45b354b252c483fb6ba8b31e85294
ksauby <ksauby@gmail.com>
2015-11-01 20:58:25 -0500

    update to PCA processing

--- R/sas_PCA_table_function.R
+++ R/sas_PCA_table_function.R
@@ -13,85 +13,182 @@
 [59] "PCA.winter.rotated.factor.pattern"                                                    
 
 
-sas_prcomp_PCA_table_function <- function(eigenvalues, factor.pattern, dataset_type="all") {
-Y <- PCA.all.surveys.eigenvalues %>%
+sas_prcomp_PCA_table_function <- function(eigenvalues, factor.pattern, dataset_type="all", round_n=3, n.axes=2) {
+X <- PCA.all.surveys.eigenvalues %>%
 	filter(Number < 3 & Eigenvalue >= 1) %>%		
 	mutate(
-		EigenNum = paste("Eigenvalue", Number, sep=""),
-		PropNum = paste("Proportion", Number, sep="")	
+		EigenNum = paste("Eigen", Number, sep=""),
+		PropNum = paste("Prop", Number, sep="")	
 	) %>%
 	.[, c("EigenNum", "PropNum", "Eigenvalue", "Proportion", "modelVars")]
-Eigens <- dcast(Y, modelVars ~ EigenNum, value.var=c("Eigenvalue"))
-PropExpl <- dcast(Y, modelVars ~ PropNum, value.var=c("Proportion"))
-# Factor 1
-FactorVals1 <- dcast(PCA.all.surveys.factor.pattern, modelVars ~ Variable, 	value.var=c("Factor1"))
-Factor1 <- 
-	base::merge(
-		FactorVals1,
-		Eigens[, c("modelVars", "Eigenvalue1")], 
-		by="modelVars"
-	) %>%
-	base::merge(PropExpl[, c("modelVars", "Proportion1")], by="modelVars") %>%
-	mutate(
-		Species = substr(modelVars,1,7),
-		Weather = str_sub(modelVars, -4, -1),
-		Axis = paste(dataset_type, "PC1")
+Eigens <- dcast(X, modelVars ~ EigenNum, value.var=c("Eigenvalue"))
+PropExpl <- dcast(X, modelVars ~ PropNum, value.var=c("Proportion"))
+# process data for each factor
+Y <- list()
+for (i in 1:n.axes) {
+	FactorVals <- dcast(
+		PCA.all.surveys.factor.pattern, 
+		modelVars ~ Variable, 
+		value.var = paste("Factor", i, sep="")
+	)
+	Y[[i]] <- 
+		merge(
+			FactorVals,
+			Eigens[, c("modelVars", paste("Eigen", i, sep=""))], 
+			by="modelVars"
+		) %>%
+		merge(
+			PropExpl[, c("modelVars", paste("Prop", i, sep=""))], 
+			by="modelVars"
+		) %>%
+		mutate(
+			Species = factor(ifelse(
+				substr(modelVars,1,7) == "HUMIFUS",
+				"O. humifusa",
+				"O. stricta"
+			)),
+			Weather = factor(ifelse(
+				str_sub(modelVars, -4, -1) == "ECIP",
+				"Precipitation",
+				"Temperature"
+			)),
+			Axis = factor(paste(dataset_type, paste("PC", i, sep="")))
 		) 
-# Factor 2
-FactorVals2 <- dcast(PCA.all.surveys.factor.pattern, modelVars ~ Variable, 
-	value.var=c("Factor2"))
-Factor2 <- base::merge(
-		FactorVals2,
-		Eigens[, c("modelVars", "Eigenvalue2")], 
-		by="modelVars"
-	) %>%
-	base::merge(PropExpl[, c("modelVars", "Proportion2")], by="modelVars") %>%
-	mutate(
-		Species = substr(modelVars,1,7),
-		Weather = str_sub(modelVars, -4, -1),
-		Axis = paste(dataset_type, "PC2")
-		) 
-		
-		rbind.fill(Factor1, Factor2)
-		
-		
-rownames(Factor1) <- Factor1[, 1]
-Factor1 %<>% .[, -1] %>% t %>% as.data.frame
-rownames(Factor2) <- Factor2[, 1]
-Factor2 %<>% .[, -1] %>% t %>% as.data.frame
-# sort into precip and temp dataframes
-# sort into stricta and humifusa dataframes
-grep("HUMIFUSA", names(Factor2))
-Factor2[ , grep("")]
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	
-	(rownames=.)
-	t %>%
-	as.data.frame
-names(Factor1) <- Factor1[1]
-	
-	
-	melt(id.vars="modelVars")
-		
-		 melt(id.vars=c("modelVars", "Number"))
-		rbind(c(
-			"Eigenvalue", 
-			PCA.all.surveys.eigenvalues$Eigenvalue[1:2] %>% round(3)
-		)) %>%
-		rbind(c(
-			"Proportion of Variance Explained",
-			summary(x)$importance[2, 1:2] %>% round(3)
-		)) %>%
-		data.table::setnames("PC1", paste(data.type, "PC1")) %>%
-		data.table::setnames("PC2", paste(data.type, "PC2"))		
-	return(y)
+	names(Y[[i]])[names(Y[[i]])==paste("Eigen", i, sep="")] <- "Eigenvalue"		
+	names(Y[[i]])[names(Y[[i]])==paste("Prop", i, sep="")] <- "Proportion"		
 }
+# merge Factor 1 and Factor 2 dataframes
+Z <- rbind.fill(Y)
+# which columns are numeric?		
+nums <- sapply(Z, is.numeric)
+Z[, nums] %<>% round(round_n)
+# filter out Axes that had Eigenvalues less than 1
+Z %<>% filter(!is.na(Eigenvalue))
+for (i in 1:dim(Z)[2]) {
+	if (substr(names(Z)[i],1,3)=="log") {
+		Z[, i] <- paste(Z[, i], "(log)")
+		names(Z)[i] <- substring(names(Z)[i], 4)
+	}
+	if (substr(names(Z)[i],1,4)=="sqrt") {
+		Z[, i] <- paste(Z[, i], "(sqrt)")
+		names(Z)[i] <- substring(names(Z)[i], 5)
+	}
+}
+Z %<>% 
+	melt(id.vars=c("modelVars", "Species", "Weather", "Axis")) %>%
+	filter(!is.na(value)) %>%		
+	.[which(grepl("NA", .$value)==FALSE), ] %>%
+	as.data.table %>%
+	unique %>%
+	reshape2::dcast(modelVars+Species+Weather+Axis ~ variable, value.var="value") %>% 
+	filter(Species=="O. humifusa" & Weather=="Precipitation")
+
+	# why did some values disappear?
+
+		
+
+######################
+
+# use this code
+# need to fix results - use sqrD1 instead of sqrtD1 - cuts off last character
+# which columns are numeric?		
+nums <- sapply(PCA.all.surveys.factor.pattern, is.numeric)
+PCA.all.surveys.factor.pattern[, nums] %<>% round(round_n)
+X <- PCA.all.surveys.factor.pattern %>% 
+	mutate(
+		#Axis = factor(paste(dataset_type, paste("PC", i, sep=""))),
+		#Number = i,
+		Factor1 = ifelse(
+			substr(Variable,1,3)=="log",
+			paste(Factor1, "(log)"),
+			Factor1
+		),
+		Factor2 = ifelse(
+			substr(Variable,1,3)=="log",
+			paste(Factor2, "(log)"),
+			Factor2
+		),
+		Variable = ifelse(
+			substr(Variable,1,3)=="log",
+			substring(Variable, 4),
+			Variable
+		)
+	) %>%
+	mutate(
+		Factor1 = ifelse(
+			substr(Variable,1,4)=="sqrt",
+			paste(Factor1, "(sqrt)"),
+			Factor1
+		),
+		Factor2 = ifelse(
+			substr(Variable,1,4)=="sqrt",
+			paste(Factor2, "(sqrt)"),
+			Factor2
+		),
+		Variable = ifelse(
+			substr(Variable,1,4)=="sqrt",
+			substring(Variable, 5),
+			Variable
+		)
+	)
+	
+	# why doesn't this work? I want to filter out NAs
+	X[which(grepl("NA", .$value)==TRUE), ]$Factor2
+temp <- dcast(X, modelVars ~ Variable, value.var="Factor1") %>%
+	mutate(Number = 1)
+Y <- dcast(X, modelVars ~ Variable, value.var="Factor2") %>%
+	mutate(Number = 2) %>%
+	merge(temp, all=T) %>%
+	mutate(		
+		Species = factor(ifelse(
+			substr(modelVars,1,7) == "HUMIFUS",
+			"O. humifusa",
+			"O. stricta"
+		)),
+		Weather = factor(ifelse(
+			str_sub(modelVars, -4, -1) == "ECIP",
+			"Precipitation",
+			"Temperature"
+		))
+	) %>%
+	merge(PCA.all.surveys.eigenvalues[, c("Number", "Eigenvalue", "Proportion", "modelVars")], by=c("Number", "modelVars"), all.x=T)
+	
+	
+	filter(Y, modelVars=="HUMIFUSATEMP")
+######################
+
+
+
+
+
+
+
+for (i in 1:length(unique(PCA.all.surveys.factor.pattern$modelVars))) {
+	for (i in 1:n.axes) {
+		PCA.all.surveys.factor.pattern %>% 
+			filter(modelVars==unique(modelVars)[i]) %>%
+			mutate(
+				Species = factor(ifelse(
+					substr(modelVars,1,7) == "HUMIFUS",
+					"O. humifusa",
+					"O. stricta"
+				)),
+				Weather = factor(ifelse(
+					str_sub(modelVars, -4, -1) == "ECIP",
+					"Precipitation",
+					"Temperature"
+				)),
+				Axis = factor(paste(dataset_type, paste("PC", i, sep="")))
+			) %>%
+			dplyr::select(-modelVars)
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			 %>%
+			dplyr::select(Variable, modelVars, eval(parse(text=paste("Factor", j, sep=""))))
