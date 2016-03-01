b80f03efe2e275ae2d3d3145153ca42b586a0d4b
ksauby <ksauby@gmail.com>
2016-03-01 13:38:26 -0500

    cleaned up lag functions

--- R/drafts/lag_functions.R
+++ R/drafts/lag_functions.R
@@ -1,13 +1,46 @@
+#' Calculate lags on a grouped dataframe
+#' @param x the dataset
+#' @param vars the vector of variables
+#' @param arrange.variable is variable that arranges the data. Defaults to "Date".
+#' @param grouping.variable is the variable that groups the data. Defaults to "PlantID".
+#' @description the function first arranges by Date
+	
+calculateLagGroupedDF <- function(
+	x, 
+	vars = vars, 
+	arrange.variable = "Date", 
+	grouping.variable = "PlantID"
+) {
+	x %<>% 
+		arrange_(.dots = arrange.variable) %>%
+		group_by_(.dots = grouping.variable)
+		# make a copy to use in the iterations
+	y <- x
+	# calculate lag for each variable in vars
+	for (i in 1:length(vars)) {
+		if (vars[i] %in% names(x)) {
+			# select columns
+			mycols <- c("Date", vars[i])
+			z <- x %>% dplyr::select(match(mycols, names(.)))
+			# set new variable name
+			var.names <- setNames(vars[i], paste0(vars[i], "_1"))
+			# calculate new lag variable
+			z %<>% mutate_each_(funs(lag), var.names)
+			y <- merge(y, z, by=c("PlantID", "Date", vars[i]))
+		}
+	}
+	return(y)
+}
+	
 #' Calculate lag dates: Previous_Survey_Date, DaysSincePrevSurvey, DaysSinceStart
 #' 
 #' @param x Dataset
 #' @description calculate lag dates.
-#' 
 #' @importFrom utils head
 #' @import chron
 #' @export
 
-lag_dates_function <- function(x){
+calculateDateLags <- function(x){
 	# print warning about duplicates in the dataset
 	duplicates <- x %>% 
 		group_by(PlantID, Date) %>%
@@ -35,14 +68,11 @@ lag_dates_function <- function(x){
 #' Calculate insect presence at previous time step
 #' 
 #' @param x Dataset
+#' @param arrange.variable is variable that arranges the data
+#' @param grouping.variable is the variable that groups the data
 #' @description calculate lag insects.
 #' 
 #' @export
-
-
-lag_insect_function
-
-Date or ObsYear
 
 calculateInsectLags <- function(
 	x=x,
@@ -63,34 +93,27 @@ calculateInsectLags <- function(
 		"Insectyr_t",
 		"NatInsectyr_t"
 	)
-	x %>% 
-		arrange(Date) %>%
-		group_by(PlantID, Date) %>%
-		mutate(
-			# new insect variables
-			Insect_t 	= ifelse(sum(DA_t, CA_t, CH_t, ME_t, na.rm=T)>0, 1, 0),
-			NatInsect_t = ifelse(sum(DA_t, CH_t, ME_t, na.rm=T)>0, 1, 0)
-		) %>%
-		group_by(PlantID) %>%
-		calculateLagGroupedDF(
-			vars=vars, 
-			arrange.variable=arrange.variable, 
-			grouping.variable=grouping.variable
-		)
+	x %>% calculateLagGroupedDF(
+		vars=vars, 
+		arrange.variable=arrange.variable, 
+		grouping.variable=grouping.variable
+	)
 }
-
-
 
 #' Calculate size variables at the previous time step
 #' 
 #' @param x Dataset
+#' @param arrange.variable is variable that arranges the data
+#' @param grouping.variable is the variable that groups the data
 #' @description calculate lag size and fruit.
 #' 
 #' @export
 
-lag_size_function <- function(x=x){
-	
-	
+calculateSizeLags <- function(
+	x=x,
+	arrange.variable, 
+	grouping.variable
+){
 	vars <- c(
 		"Size_t", 
 		"Height_t", 
@@ -98,81 +121,27 @@ lag_size_function <- function(x=x){
 		"Cylinder_Tall_t", 
 		"Cylinder_t", 
 		"Elliptic_Cylinder_t"
+		"Size_max_t",
+		"Size_min_t",
+		"Cone_max_t",
+		"Cylinder_Tall_max_t"
 	)
-	
-	
-	x <- Plant_Surveys
-
-	#' @param x the dataset
-	#' @param vars the vector of variables
-	#' @param arrange.variable is variable that arranges the data. Defaults to "Date".
-	#' @param grouping.variable is the variable that groups the data. Defaults to "PlantID".
-	#' @description the function first arranges by Date
-	
-	calculateLagGroupedDF <- function(
-		x, 
-		vars = vars, 
-		arrange.variable = "Date", 
-		grouping.variable = "PlantID"
-	) {
-		x %<>% 
-			arrange_(.dots = arrange.variable) %>%
-			group_by_(.dots = grouping.variable)
-			# make a copy to use in the iterations
-		y <- x
-		# calculate lag for each variable in vars
-		for (i in 1:length(vars)) {
-			if (vars[i] %in% names(x)) {
-				# select columns
-				mycols <- c("Date", vars[i])
-				z <- x %>% dplyr::select(match(mycols, names(.)))
-				# set new variable name
-				var.names <- setNames(vars[i], paste0(vars[i], "_1"))
-				# calculate new lag variable
-				z %<>% mutate_each_(funs(lag), var.names)
-				y <- merge(y, z, by=c("PlantID", "Date", vars[i]))
-			}
-		}
-		return(y)
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	vars <- c(
-		"Size_t", 
-		"Height_t", 
-		"Cone_t", 
-		"Cylinder_Tall_t", 
-		"Cylinder_t", 
-		"Elliptic_Cylinder_t"
-		"Size_max_t_1",
-		"Size_min_t_1",
-		"Cone_max_t_1",
-		"Cylinder_Tall_max_t_1"
+	x %>% calculateLagGroupedDF(
+		vars=vars, 
+		arrange.variable=arrange.variable, 
+		grouping.variable=grouping.variable
 	)
-	
-	
-		
+}
 
-
-#' Calculate size and fruit production variables at the previous time step
+#' Calculate fruit production variables at the previous time step
 #' 
 #' @param x Dataset
+#' @param arrange.variable is variable that arranges the data
+#' @param grouping.variable is the variable that groups the data
+
 #' @description calculate lag size and fruit.
 #' 
 #' @export
-lag_size_fruit_function
 
 calculateFruitLags <- function(
 	x=x,
@@ -190,30 +159,5 @@ calculateFruitLags <- function(
 			vars=vars, 
 			arrange.variable=arrange.variable, 
 			grouping.variable=grouping.variable
-		)
-}
-
-
-#' Calculate size and fruit production variables at the previous time step
-#' 
-#' @param x Dataset
-#' @description calculate lag size and fruit.
-#' 
-#' @export
-
-lag_size_fruit_function_GTMNERR <- function(x=x){
-	x %>% 
-		arrange(Date) %>%
-		group_by(PlantID) %>%
-		mutate(
-			# size
-			Size_t_1 				= c(NA, head(Size_t, -1)),
-			Height_t_1 				= c(NA, head(Height_t, -1)),
-			Cone_t_1 				= c(NA, head(Cone_t, -1)),
-			Cylinder_t_1 			= c(NA, head(Cylinder_t, -1)),
-			Elliptic_Cylinder_t_1 	= c(NA, head(Elliptic_Cylinder_t, -1)),
-			# fruit
-			Fruit_t_1 			= c(NA, head(Fruit_t, -1)),
-			FruitPres_t_1 		= c(NA, head(FruitPres_t, -1))
 		)
 }
