d38db1b760671bc965377a54383ce0060ccd35dc
ksauby <ksauby@gmail.com>
2016-03-01 13:38:45 -0500

    created stand alone insect functions

---
+++ R/InsectFunctions.R
@@ -0,0 +1,20 @@
+#' Calculate Insect Variables
+#' @param x the dataset
+#' @param arrange.variable is variable that arranges the data. Defaults to "Date".
+#' @param grouping.variable is the variable that groups the data. Defaults to "PlantID".
+#' @description Calculate insects at time t ("Insect_t") and native insects at time t ("NatInsect_t").
+
+createNewInsectVariables <- function(
+	x, 
+	arrange.variable = "Date", 
+	grouping.variable = "PlantID"
+) {
+	x %>%
+		arrange_(.dots=arrange.variable) %>%
+		group_by_(.dots=c(arrange.variable, grouping.variable)) %>%
+		mutate(
+			# new insect variables
+			Insect_t 	= ifelse(sum(DA_t, CA_t, CH_t, ME_t, na.rm=T)>0, 1, 0),
+			NatInsect_t = ifelse(sum(DA_t, CH_t, ME_t, na.rm=T)>0, 1, 0)
+		)
+}
