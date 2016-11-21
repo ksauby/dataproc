f6e3bb65bf5129ac76037ad1be07cba70c14e93f
ksauby <ksauby@gmail.com>
2016-11-20 21:22:08 -0500

    updates to variance calc

--- R/Var.R
+++ R/Var.R
@@ -1,7 +1,20 @@
-#' Return the variance of a vector with up to two decimal places, after removing NAs.
+#' Return the sample variance of a vector, after removing NAs, and round.
 #'
 #' @param x Vectors of data.
+#' @param round_n Number of digits to round to.
 #' @description Written for use in the tables::tabular function to create publication-ready tables.
 #' @export
 
 Variance <- function(x, round_n=3) stats::var(x, na.rm=TRUE) %>% round(round_n)
+
+#' Return the population variance of a vector, after removing NAs, and round.
+#'
+#' @param x Vectors of data.
+#' @param round_n Number of digits to round to.
+#' @description Written for use in the tables::tabular function to create publication-ready tables.
+#' @export
+
+PopVariance <- function(x, round_n=3) {
+	temp <- sum((x-mean(x))^2)/length(x)
+	round(temp, round_n)
+}
