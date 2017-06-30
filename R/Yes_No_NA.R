24f367d8ac2d69fa2c5c3f492f49ad1dd26375af
ksauby <ksauby@gmail.com>
2017-06-29 21:34:33 -0400

    updates to some functions

--- R/Yes_No_NA.R
+++ R/Yes_No_NA.R
@@ -28,6 +28,18 @@ Replace_1_w_Yes_Function <- function(x){
 #' @export
 
 Replace_NA_w_Period_Function <- function(x){
+	x[is.na(x) == "TRUE"] <- "."
+	return(x)
+}
+
+#' Replace NA with "".
+#'
+#' @param x Vector of data.
+#' @description Replace NAs with no text so they are not printed on datasheets.
+#'
+#' @export
+
+Replace_NA_w_Blank_Function <- function(x){
 	x[is.na(x) == "TRUE"] <- ""
 	return(x)
 }
