4ad15c7114db4e805d1c6b79a9ae0883ff82e172
ksauby <ksauby@gmail.com>
2019-01-28 11:04:13 -0500

    clean up using devtools check

--- R/map_display.R
+++ R/map_display.R
@@ -145,22 +145,86 @@ createScaleBar <- function(lon,lat,distanceLon,distanceLat,distanceLegend, dist.
 #' 
 #' @export
 
-scaleBar <- function(lon, lat, distanceLon, distanceLat, distanceLegend, dist.unit = "km", rec.fill = "white", rec.colour = "black", rec2.fill = "black", rec2.colour = "black", legend.colour = "black", legend.size = 3, orientation = TRUE, arrow.length = 500, arrow.distance = 300, arrow.North.size = 6){
-	laScaleBar <- createScaleBar(lon = lon, lat = lat, distanceLon = distanceLon, distanceLat = distanceLat, distanceLegend = distanceLegend, dist.unit = dist.unit)
+scaleBar <- function(
+	lon, 
+	lat, 
+	distanceLon, 
+	distanceLat, 
+	distanceLegend, 
+	dist.unit = "km", 
+	rec.fill = "white", 
+	rec.colour = "black", 
+	rec2.fill = "black", 
+	rec2.colour = "black", 
+	legend.colour = "black", 
+	legend.size = 3, 
+	orientation = TRUE, 
+	arrow.length = 500, 
+	arrow.distance = 300, 
+	arrow.North.size = 6
+){
+	laScaleBar <- createScaleBar(
+		lon = lon, 
+		lat = lat, 
+		distanceLon = distanceLon, 
+		distanceLat = distanceLat, 
+		distanceLegend = distanceLegend, 
+		dist.unit = dist.unit
+	)
 	# First rectangle
-	rectangle1 <- geom_polygon(data = laScaleBar$rectangle, aes(x = lon, y = lat), fill = rec.fill, colour = rec.colour)
+	rectangle1 <- geom_polygon(
+		data = laScaleBar$rectangle, 
+		aes(x = lon, y = lat), 
+		fill = rec.fill, 
+		colour = rec.colour
+	)
 
 	# Second rectangle
-	rectangle2 <- geom_polygon(data = laScaleBar$rectangle2, aes(x = lon, y = lat), fill = rec2.fill, colour = rec2.colour)
+	rectangle2 <- geom_polygon(
+		data = laScaleBar$rectangle2, 
+		aes(x = lon, y = lat), 
+		fill = rec2.fill, 
+		colour = rec2.colour
+	)
 
 	# Legend
-	scaleBarLegend <- annotate("text", label = paste(laScaleBar$legend[,"text"], dist.unit, sep=""), x = laScaleBar$legend[,"long"], y = laScaleBar$legend[,"lat"], size = legend.size, colour = legend.colour)
+	scaleBarLegend <- annotate(
+		"text", 
+		label = paste(laScaleBar$legend[,"text"], dist.unit, sep=""), 
+		x = laScaleBar$legend[,"long"], 
+		y = laScaleBar$legend[,"lat"], 
+		size = legend.size, 
+		colour = legend.colour
+	)
 
 	res <- list(rectangle1, rectangle2, scaleBarLegend)
 
 	if(orientation){# Add an arrow pointing North
-		coordsArrow <- createOrientationArrow(scaleBar = laScaleBar, length = arrow.length, distance = arrow.distance, dist.unit = dist.unit)
-		arrow <- list(geom_segment(data = coordsArrow$res, aes(x = x, y = y, xend = xend, yend = yend)), annotate("text", label = "N", x = coordsArrow$coordsN[1,"x"], y = coordsArrow$coordsN[1,"y"], size = arrow.North.size, colour = "black"))
+		coordsArrow <- createOrientationArrow(
+			scaleBar = laScaleBar, 
+			length = arrow.length, 
+			distance = arrow.distance, 
+			dist.unit = dist.unit
+		)
+		arrow <- list(
+			geom_segment(
+				data = coordsArrow$res, 
+				aes(
+					x = .data$x, 
+					y = .data$y, 
+					xend = .data$xend, 
+					yend = .data$yend
+				)
+			), 
+			annotate(
+				"text", 
+				label = "N", 
+				x = coordsArrow$coordsN[1,"x"], 
+				y = coordsArrow$coordsN[1,"y"], 
+				size = arrow.North.size, 
+				colour = "black"
+			)
+		)
 		res <- c(res, arrow)
 	}
 	return(res)
